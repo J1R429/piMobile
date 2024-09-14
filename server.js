e05971dc761c103bf8d88adfd76c0feea93d8f12
J1R429 <jessica.fernandes@adv.oabsp.org.br>
2024-09-14 00:15:57 -0300

    css pag login

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const port = 3000;
 // Configuração do EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
